fbb3f247913f3fb1c750616ae496b598a75aea6a
dimatolshin <dima_tolshin@mail.ru>
2023-06-10 01:32:10 +0300

    hm

--- internet_shop/models.py
+++ internet_shop/models.py
@@ -2,12 +2,20 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-
 class Category(models.Model):
     name = models.CharField(max_length=100)
+    image = models.CharField(max_length=1000, blank=True)
 
     def __str__(self):
         return self.name
+
+    @property
+    def count(self):
+        count = 0
+        category = Category.objects.all()
+        for i in category:
+            count += 1
+        return count
 
 
 class Product(models.Model):
@@ -16,6 +24,7 @@ class Product(models.Model):
     price = models.FloatField(default=0.0)
     category = models.ForeignKey(Category, on_delete=models.CASCADE,
                                  related_name='products')
+    image = models.CharField(max_length=1000, blank=True)
 
     def __str__(self):
         return self.name
@@ -63,7 +72,3 @@ class Profile(models.Model):
 
     def __str__(self):
         return str(self.user)
-
-
-
-
